939f4f172277f6aa5f597dc7d39c44fb62f416a4
Marcilio128 <marciliogomes2005@gmail.com>
2022-07-26 18:37:08 -0300

    criação da parte inicial da welcome

--- Challenge3e/resources/views/welcome.blade.php
+++ Challenge3e/resources/views/welcome.blade.php
@@ -3,3 +3,26 @@
 @section('title', '3E')
 
 @section('content')
+<section class="sec-initial">
+    <div class="container">
+        <div class="text-initial">
+            <h1>Bem vindo ao dia dos pais na 3e engenharia</h1>
+            <p>O dia dos pais é o dia de homenagearmos aquele que é nosso grande exemplo,
+                que está conosco em todas as horas, que nunca nos deixou faltar amor, carinho 
+                e que sempre nós compreendeu, venha homenagear seu grande herói!!! 
+            </p>
+        </div>
+        <lottie-player class="animation-initial" src="https://assets4.lottiefiles.com/packages/lf20_9e8yoqkm.json"  background="transparent"  speed="1"   loop  autoplay></lottie-player>   
+     </div>
+</section>
+
+<h1>Conheça esses grandes pais</h1>
+
+<section class="">
+    <div classe="container">
+        <div>
+            <img src="/assets/alan-turng" alt="">
+        </div>
+    </div>
+</section>
+@endsection
